93e1826d9242bfb727de7519a4ec7e09c032cf0d
Ahmed Salah <asamee224488@gmail.com>
2025-03-03 00:38:48 +0200

    Refactor MyFriendsScreen to use MyFriendsGrid for better code organization and maintainability

--- lib/feature/myFriends/my_friends_screen.dart
+++ lib/feature/myFriends/my_friends_screen.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/core/movie_app_bar.dart';
 import 'package:movie_app/core/my_colors.dart';
-import 'package:movie_app/core/my_images.dart';
 import 'package:movie_app/core/my_styles.dart';
 import 'package:movie_app/core/my_text.dart';
 import 'package:movie_app/core/spacing.dart';
-import 'package:movie_app/feature/auth/widget/my_text_btns.dart';
+import 'package:movie_app/feature/myFriends/widget/my_friends_grid.dart';
 
 class MyFriendsScreen extends StatelessWidget {
   const MyFriendsScreen({super.key});
@@ -33,62 +32,9 @@ class MyFriendsScreen extends StatelessWidget {
                 decoration: BoxDecoration(
                     color: MyColors.secondaryColor,
                     borderRadius: BorderRadius.circular(30)),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 50),
-                  child: GridView.builder(
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: 18,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            childAspectRatio: 0.90,
-                            crossAxisSpacing: 10,
-                            mainAxisSpacing: 10,
-                            mainAxisExtent: 200,
-                            crossAxisCount: 3),
-                    itemBuilder: (context, index) {
-                      return Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            children: [
-                              Image.asset(MyImages.myFriends),
-                              hSpace(20),
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  const Text(
-                                    MyText.patrickBatman,
-                                    style: MyStyles.title24White400,
-                                  ),
-                                  vSpace(20),
-                                  Row(
-                                    children: [
-                                      Text(
-                                        '${MyText.friends} :54',
-                                        style: MyStyles.title24White400
-                                            .copyWith(fontSize: 16),
-                                      ),
-                                      hSpace(20),
-                                      Text(
-                                        '${MyText.watched} :187',
-                                        style: MyStyles.title24White400
-                                            .copyWith(fontSize: 16),
-                                      ),
-                                    ],
-                                  ),
-                                  vSpace(20),
-                                  MyTextBtn(
-                                      onTap: () {},
-                                      text: MyText.add,
-                                      color: Colors.blue,
-                                      textColor: Colors.white,
-                                      radius: 20)
-                                ],
-                              )
-                            ],
-                          ));
-                    },
-                  ),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 50),
+                  child: MyFriendsGrid(),
                 )),
             vSpace(50),
           ],
---
+++ lib/feature/myFriends/widget/my_friends_grid.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/core/my_images.dart';
+import 'package:movie_app/core/my_styles.dart';
+import 'package:movie_app/core/my_text.dart';
+import 'package:movie_app/core/spacing.dart';
+import 'package:movie_app/feature/auth/widget/my_text_btns.dart';
+
+class MyFriendsGrid extends StatelessWidget {
+  const MyFriendsGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: 18,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          childAspectRatio: 0.90,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          mainAxisExtent: 200,
+          crossAxisCount: 3),
+      itemBuilder: (context, index) {
+        return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Image.asset(MyImages.myFriends),
+                hSpace(20),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      MyText.patrickBatman,
+                      style: MyStyles.title24White400,
+                    ),
+                    vSpace(20),
+                    Row(
+                      children: [
+                        Text(
+                          '${MyText.friends} :54',
+                          style:
+                              MyStyles.title24White400.copyWith(fontSize: 16),
+                        ),
+                        hSpace(20),
+                        Text(
+                          '${MyText.watched} :187',
+                          style:
+                              MyStyles.title24White400.copyWith(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                    vSpace(20),
+                    MyTextBtn(
+                        onTap: () {},
+                        text: MyText.add,
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                        radius: 20)
+                  ],
+                )
+              ],
+            ));
+      },
+    );
+  }
+}
